35c6fc28c73b165439c7d6be2375dd0d5d42516a
Nicolas Chan <nicolas@nicolaschan.com>
2017-03-06 11:03:57 -0800

    fixed genetic algorithm so it gets better instead of worse every time

--- src/main/java/com/nicolaschan/asi/algorithms/Genome.java
+++ src/main/java/com/nicolaschan/asi/algorithms/Genome.java
@@ -1,5 +1,7 @@
 package com.nicolaschan.asi.algorithms;
 
+import java.util.Random;
+
 /**
  * Created by nicol on 2/16/2017.
  */
@@ -26,5 +28,33 @@ public class Genome {
             out.append((i == values.length - 1) ? values[i] : values[i] + ",");
 
         return out + "]";
+    }
+
+    public static Genome generateRandomGenome(long seed) {
+        return generateRandomGenome(seed, 8);
+    }
+
+    public static Genome generateRandomGenome(long seed, int size) {
+        return generateRandomGenome(seed, size, 0, 8);
+    }
+
+    public static Genome generateRandomGenome(long seed, int size, int lowerBound, int upperBound) {
+        Random random = new Random(seed);
+        int[] output = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            output[i] = lowerBound + (int) (random.nextDouble() * (upperBound - lowerBound));
+        }
+
+        return new Genome(output);
+    }
+
+    public static Genome[] generateRandomGenomes(long seed, int size) {
+        Genome[] genomes = new Genome[size];
+        Random random = new Random(seed);
+        for (int i = 0; i < size; i++) {
+            genomes[i] = Genome.generateRandomGenome(random.nextInt());
+        }
+        return genomes;
     }
 }
